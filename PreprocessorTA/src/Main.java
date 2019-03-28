709d13c519c4fdf40a84e3fdfd5dea5e113fe26f
FAIQ-ROG <karyoutomoo@gmail.com>
2019-03-29 00:53:42 +0700

    added actor list of Pahlawan Nasional

--- PreprocessorTA/src/Main.java
+++ PreprocessorTA/src/Main.java
@@ -1,16 +1,12 @@
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
+
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.rdf.model.impl.StmtIteratorImpl;
 import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.varia.NullAppender;
-import com.hp.hpl.jena.util.iterator.Filter;
 import org.mindswap.pellet.jena.PelletReasonerFactory;
 import java.io.*;
 
@@ -36,21 +32,118 @@ public class Main {
         File fileRDF = new File("D:\\The-Tree-of-Heroes\\PreprocessorTA\\result.rdf");
 
         BasicConfigurator.configure(new NullAppender());
-        final OntModel ontModel = ModelFactory.createOntologyModel( OntModelSpec.OWL_DL_MEM );
         System.out.println("Apache Jena Modelling, Reasoning and Inferring Tool");
 
-                                 // MEMODELKAN FILE ACTOR DARI OWL ONTOLOGI FAMILY dan JENA-FUSEKI
+                                 //MEMODELKAN FILE ACTOR DARI OWL ONTOLOGI FAMILY dan JENA-FUSEKI
         FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
         Model Instances = FileManager.get().loadModel(READ_FUSEKI);
-        Instances.read(READ_FUSEKI,"TTL");
+        Instances.read(READ_FUSEKI,"RDF/XML");
 
         Model famonto = FileManager.get().loadModel(OWL_FILE_LOCATION);
+
+                                //MERGING MODEL DARI JENA-FUSEKI DAN MODEL ONTOLOGI FAMILY
+        final Model union = Instances;
 
                                 //ADD ACTOR
         FileManager fManager = FileManager.get();
         fManager.addLocatorURL();
-
-        String actor = "Abdurrahman_Wahid";
+        /*
+        LIST ACTOR
+        Soekarno
+        Mohammad_Hatta
+        Abdul_Haris_Nasution
+        Adam_Malik
+        Ahmad_Yani
+        Basuki_Rahmat
+        Tjipto_Mangoenkoesoemo
+        Oemar_Said_Tjokroaminoto
+        Ernest_Douwes_Dekker
+        Dewi_Sartika
+        Cut_Nyak_Dhien
+        Diponegoro
+        Fatmawati
+        Ferdinand_Lumbantobing
+        Frans_Kaisiepo
+        Gatot_Soebroto
+        Halim_Perdanakusuma
+        Hamengkubuwana_I
+        Hamengkubuwana_IX
+        Hasjim_Asy%27ari
+        Sultan_Hasanuddin
+        Tuanku_Imam_Bonjol
+        Iskandar_Muda
+        Ismail_Marzuki
+        Iswahjoedi
+        Radin_Inten_II
+        Johannes_Abraham_Dimara
+        Djoeanda_Kartawidjaja
+        Karel_Satsuit_Tubun
+        Kartini
+        Kasman_Singodimedjo
+        I_Gusti_Ketut_Jelantik
+        Ki_Hadjar_Dewantara
+        Sultan_Mahmud_Badaruddin_II
+        Malahayati
+        Maria_Walanda_Maramis
+        Martha_Christina_Tiahahu
+        Mas_Tirtodarmo_Haryono
+        Cut_Nyak_Meutia
+        Mohammad_Husni_Thamrin
+        Mohammad_Natsir
+        Teuku_Muhammad_Hasan
+        Mohammad_Yamin
+        Muhammad_Yasin
+        Muhammad_Zainuddin_Abdul_Madjid
+        Moestopo
+        Nani_Wartabone
+        I_Gusti_Ngurah_Rai
+        Nuku_Muhammad_Amiruddin
+        Oto_Iskandar_di_Nata
+        Pakubuwana_VI
+        Pakubuwana_X
+        Pattimura
+        Pierre_Tendean
+        Raja_Haji_Fisabilillah
+        Rasuna_Said
+        Radjiman_Wedyodiningrat
+        Sam_Ratulangi
+        Samanhudi
+        Sisingamangaraja_XII
+        Siswondo_Parman
+        Slamet_Rijadi
+        Soedirman
+        Albertus_Soegijapranata
+        Suharso
+        Soekarni
+        Sultan_Agung_dari_Mataram
+        Andi_Sultan_Daeng_Radja
+        Soepeno
+        R._Suprapto_(pahlawan_revolusi)
+        Soeprijadi
+        Soeroso
+        Ario_Soerjo
+        Sutan_Syahrir
+        Soetomo
+        Sutomo
+        Sutoyo_Siswomiharjo
+        Syafruddin_Prawiranegara
+        Syam%27un
+        Syarif_Kasim_II_dari_Siak
+        T.B._Simatupang
+        Tan_Malaka
+        Tirto_Adhi_Soerjo
+        Teuku_Umar
+        Untung_Suropati
+        Oerip_Soemohardjo
+        Wage_Rudolf_Soepratman
+        Wahid_Hasjim
+        Wahidin_Soedirohoesodo
+        Wilhelmus_Zakaria_Johannes
+        Yos_Sudarso
+        Zainul_Arifin
+        Zainal_Mustafa
+         */
+        String actor = "Joko_Widodo";
         Model modelActor = fManager.loadModel("http://id.dbpedia.org/data/"+actor+".rdf");
         final Resource actorResource = modelActor.getResource(res + actor);
         final Property hasSpouse = modelActor.getProperty(prop + "spouse");
@@ -61,16 +154,14 @@ public class Main {
         while ( stmtIteratorSpouse.hasNext() ){
             Statement spouse = stmtIteratorSpouse.nextStatement();
             System.out.println( "ACTOR SPOUSE"+spouse );
-            Instances.add(spouse);
+            union.add(spouse);
         }
         stmtIteratorChild = modelActor.listStatements(actorResource,hasChildren,(RDFNode)null);
         while ( stmtIteratorChild.hasNext() ) {
             Statement child = stmtIteratorChild.nextStatement();
             System.out.println( "ACTOR CHILDREN"+child );
-            Instances.add(child);
+            union.add(child);
         }
-                                // MERGING MODEL DARI JENA-FUSEKI DAN MODEL ONTOLOGI FAMILY
-        final Model union = ModelFactory.createUnion(Instances,famonto);
 
                                 // REASONING MODEL UNION
         Reasoner pelletReasoner = PelletReasonerFactory.theInstance().create();
@@ -107,41 +198,28 @@ public class Main {
             System.out.println("End Query");
         }
 
-                                // EKSTRAKSI INFERRED MODEL ONLY
-        System.out.println("Getting Inferred Model..");
-        ExtendedIterator<Statement> stmts = reasonedModel.listStatements().filterDrop( new Filter<Statement>(){
-            public boolean accept(Statement o){
-                return Instances.contains(o);
-            }
-        });
 
-                                // KONVERSI KE FILE .RDF
-        Model deductions = ModelFactory.createDefaultModel().add( new StmtIteratorImpl( stmts ));
-
+                                /*KONVERSI KE FILE .RDF*/
         if(fileRDF.delete())
         {
             System.out.println("The old result.rdf file deleted successfully");
         }
-        else
-        {
-            System.out.println("Creating new result as RDF File");
-        }
+        else System.out.println("Creating new result as RDF File");
+
         PrintStream fileStream = new PrintStream("result.rdf");
         System.setOut(fileStream);
 
-        deductions.write( System.out, "RDF/XML" );
+        reasonedModel.write( System.out, "RDF/XML" );
 
-                                // UPLOAD TO JENA FUSEKI
-        // parse the file
+                            // READ FILE LOCATION
         try (FileInputStream in = new FileInputStream(fileRDF)) {
-            deductions.read(in, null, "RDF/XML");
+            reasonedModel.read(in, null, "RDF/XML");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // upload the resulting model
+                            // UPLOAD TO JENA FUSEKI
         DatasetAccessor accessor = DatasetAccessorFactory
                 .createHTTP(UPLOAD_FUSEKI);
-        accessor.add(deductions);
+        accessor.putModel(reasonedModel);
     }
 }
